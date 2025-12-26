16a09b2cf34612b3af1f91899d6d86e781619e64
joaovcalvet <joaovictor.calvet@outlook.com>
2025-12-26 03:29:55 -0300

    feat(users): add user component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from './components/legacy/header/header.component';
+import { UserComponent } from './components/legacy/user/user.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
