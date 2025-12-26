c54daf4cb9374098a5b5876f668cbaa301820380
joaovcalvet <joaovictor.calvet@outlook.com>
2025-12-26 06:03:21 -0300

    feat(users): add events to component

--- src/app/components/legacy/user/user.component.ts
+++ src/app/components/legacy/user/user.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../../../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
-  selector: 'app-user',
-  standalone: true,
-  imports: [],
-  templateUrl: './user.component.html',
-  styleUrl: './user.component.css',
+	selector: 'app-user',
+	standalone: true,
+	imports: [],
+	templateUrl: './user.component.html',
+	styleUrl: './user.component.css',
 })
-export class UserComponent { }
+export class UserComponent {
+	public selectedUser = DUMMY_USERS[randomIndex];
+
+	public get imagePath(): string {
+		return `users/${this.selectedUser.avatar}`;
+	}
+
+	public onSelectUser(): void {
+		this.selectedUser = DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)];
+	}
+}
